d0646e8c20c121d8473a9223b3dc76d49f4d035d
Edward Castaneda <aeghin7@gmail.com>
2021-10-27 23:25:13 -0500

    final version

---
+++ routes/notes.js
@@ -0,0 +1,70 @@
+const notes = require('express').Router();
+const fs = require('fs');
+
+notes.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) { console.log(err); }
+        else {
+            res.json(JSON.parse(data));
+        }
+    })
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) { console.log(err); }
+        else {
+
+            let newData = JSON.parse(data);
+            let newID = 1;
+            console.log(newData);
+            if (newData.length !== 0) {
+                newID = newData[newData.length - 1].id + 1;
+            }
+            const newNote = {
+                id: newID,
+                title: title,
+                text: text,
+            }
+            newData.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(newData, null, 4), (err) => {
+                if (err) { console.error(err); }
+                else {
+                    res.json(newData);
+                    console.info('\nNew note has been written');
+                }
+            })
+        };
+
+    }
+    )
+});
+
+
+notes.delete('/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) { console.log(err); }
+        else {
+            let nData = JSON.parse(data);
+            let newData = nData.filter(nata => nata.id != req.params.id);
+            console.log(req.params.id);
+            console.log(newData);
+            fs.writeFile('./db/db.json', JSON.stringify(newData, null, 4), (err) => {
+                if (err) { console.error(err); }
+                else {
+                    res.json(newData);
+                    console.info('\nNote has been Deleted');
+                }
+            })
+        };
+
+    }
+    )
+})
+
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
-const htmlRoutes = require('./routes/home_routes');
-const apiRoutes = require('./routes/api_routes');
+const path = require('path');
+const api = require('./routes/index.js');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.use(apiRoutes);
-app.use(htmlRoutes);
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
